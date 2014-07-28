e07bb3c50eaaa1c60870122c7e64312d60832dbf
LeeHey <lcxfs1991@gmail.com>
2014-07-29 03:42:37 +0800

    index page
    index page

--- homepage/views.py
+++ homepage/views.py
@@ -3,11 +3,13 @@ from django.template import RequestContext, loader
 from homepage.models import Article
 
 def index(request):
-	article_list=Article.objects.order_by('id')[:5]
+	article_list=Article.objects.exclude(main_title='About Me')
+	about_me=Article.objects.filter(main_title='About Me')
 
 	template=loader.get_template('homepage/index.html')
 	context=RequestContext(request, {
 	        'article_list': article_list,
+	        'about_me': about_me,
 			})
     
     	return HttpResponse(template.render(context))
