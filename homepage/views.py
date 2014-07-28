539658b436b0ecf3cf3920a8e7eecc17fe930e44
LeeHey <lcxfs1991@gmail.com>
2014-07-28 23:26:57 +0800

    2nd commit
    2nd commit

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from homepage.models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+	article_list=Article.objects.order_by('id')[:5]
+
+	template=loader.get_template('homepage/index.html')
+	context=RequestContext(request, {
+	        'article_list': article_list,
+			})
+    
+    	return HttpResponse(template.render(context))
