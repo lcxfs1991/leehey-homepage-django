20559a7519c11b5d777d136749d716d6801ac133
LeeHey <lcxfs1991@gmail.com>
2014-08-02 01:49:24 +0800

    project
    project

--- homepage/views.py
+++ homepage/views.py
@@ -8,23 +8,31 @@ from django.core.mail import send_mail
 def index(request):
 	article_list=Article.objects.exclude(main_title='About Me')
 	about_me=Article.objects.filter(main_title='About Me')
-	project_list=Project.objects.all()
+	project_list1=Project.objects.all()[:3]
+	project_list2=Project.objects.all()[3:6]
 
 	template=loader.get_template('homepage/index.html')
 
-	if (project_list):
+	if (project_list1):
 		i = 0
-		for project in project_list:
-    			project_list[i].pic_path.name = project_list[i].pic_path.name.replace('homepage/static/', '')
-    			i += 1
-
-
+		for project in project_list1:
+				project_list1[i].pic_path.name = project_list1[i].pic_path.name.replace('homepage/static/', '')
+				i += 1
+	
+	if (project_list2):
+		i = 0
+		for project in project_list2:
+				project_list2[i].pic_path.name = project_list2[i].pic_path.name.replace('homepage/static/', '')
+				i += 1
+	
 	context=RequestContext(request, {
 		'article_list': article_list,
 		'about_me': about_me,
-		'project_list': project_list,
+		'project_list1': project_list1,
+		'project_list2': project_list2,
 		'menu': 'home',
 	})
+
 	return HttpResponse(template.render(context))
 
 def contact(request):
@@ -37,15 +45,15 @@ def contact(request):
 		contactForm = ContactForm(request.POST)
 
 		if contactForm.is_valid():
-		    subject = contactForm.cleaned_data['subject']
-		    message = contactForm.cleaned_data['message']
-		    sender = contactForm.cleaned_data['sender']
-		    recipients = ['lcxfs1991@gmail.com']
+			subject = contactForm.cleaned_data['subject']
+			message = contactForm.cleaned_data['message']
+			sender = contactForm.cleaned_data['sender']
+			recipients = ['lcxfs1991@gmail.com']
 
-		    if send_mail(subject, message, sender, recipients, fail_silently=False):
-		    	emailResult = "Your message has been successfully sent to me! Thank you!"
-		    else:
-		    	emailResult = "Your message fails to be sent!"
+			if send_mail(subject, message, sender, recipients, fail_silently=False):
+				emailResult = "Your message has been successfully sent to me! Thank you!"
+			else:
+				emailResult = "Your message fails to be sent!"
 
 		else:
 			emailResult = "Your submit information has not been completed"
